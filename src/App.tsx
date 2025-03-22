9c48259fe9d85749cc2db45c66751bee4b1d12ea
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-22 19:30:20 -0300

    Split Footer from App to enable better composability and better maintenance

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   const workInProgressIcon = "./work-in-progress-color.png";
@@ -14,35 +15,7 @@ function App() {
         </p>
         <p>Website in construction</p>
       </div>
-      <footer>
-        <p>
-          Developed by Dickson Souza @2025 | &nbsp;
-          <a
-            href="https://www.linkedin.com/in/disouzam"
-            target="_blank"
-            rel="noopener"
-          >
-            LinkedIn profile
-          </a>
-          &nbsp;|&nbsp;
-          <a
-            href="https://github.com/disouzam"
-            target="_blank"
-            rel="noopener"
-          >
-            GitHub profile
-          </a>
-        </p>
-
-        <p>
-          <a
-            href="https://www.flaticon.com/free-icons/under-construction"
-            title="under-construction icons"
-          >
-            Under-construction icons created by Freepik - Flaticon
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+function Footer() {
+    return (
+        <footer>
+        <p>
+          Developed by Dickson Souza @2025 | &nbsp;
+          <a
+            href="https://www.linkedin.com/in/disouzam"
+            target="_blank"
+            rel="noopener"
+          >
+            LinkedIn profile
+          </a>
+          &nbsp;|&nbsp;
+          <a
+            href="https://github.com/disouzam"
+            target="_blank"
+            rel="noopener"
+          >
+            GitHub profile
+          </a>
+        </p>
+
+        <p>
+          <a
+            href="https://www.flaticon.com/free-icons/under-construction"
+            title="under-construction icons"
+          >
+            Under-construction icons created by Freepik - Flaticon
+          </a>
+        </p>
+      </footer>
+    );
+}
+
+export default Footer;
