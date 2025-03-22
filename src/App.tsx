99d8c327ee8a689cdded8f358f2d2f7ff702cadf
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-22 14:11:40 -0300

    Moved footer to App instead of relying on separate html tag in index.html file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div>Website in construction</div>
+      <footer>
+        <p>© 2025 Dickson Souza</p>
+        <p>
+          <a
+            href="https://www.flaticon.com/free-icons/under-construction"
+            title="under-construction icons"
+          >
+            Under-construction icons created by Freepik - Flaticon
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://www.flaticon.com/free-icons/excavation"
+            title="excavation icons"
+          >
+            Excavation icons created by IconBaandar - Flaticon
+          </a>
+        </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
