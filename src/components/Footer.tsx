5f2c96b1e6978be294c0eca53f2a1d0d483211f5
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-30 20:46:49 -0300

    Adopted Bootstrap styles in About text and footer
    - Still figuring out how to work with Bootstrap
    - Useful documentation at their website is being helpful though the number of options is overwhelming
    - https://getbootstrap.com/docs/5.3/getting-started/introduction/
    - Also a set of examples (more specifically the Blog template is being used as a guide to take classes from there
    - https://getbootstrap.com/docs/5.3/examples/

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,35 +1,36 @@
 function Footer() {
-    return (
-        <footer>
-        <p>
-          Developed by Dickson Souza @2025 | &nbsp;
-          <a
-            href="https://www.linkedin.com/in/disouzam"
-            target="_blank"
-            rel="noopener"
-          >
-            LinkedIn profile
-          </a>
-          &nbsp;|&nbsp;
-          <a
-            href="https://github.com/disouzam"
-            target="_blank"
-            rel="noopener"
-          >
-            GitHub profile
-          </a>
-        </p>
+  return (
+    <footer className="py-8 text-center text-body-secondary bg-body-tertiary col-md-6 mx-auto">
+      <p>Developed by Dickson Souza @2025</p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/disouzam"
+          target="_blank"
+          rel="noopener"
+        >
+          LinkedIn profile
+        </a>
+        &nbsp;|&nbsp;
+        <a href="https://github.com/disouzam" target="_blank" rel="noopener">
+          GitHub profile
+        </a>
+      </p>
 
-        <p className="footer-icon-attribution">
-          <a
-            href="https://www.flaticon.com/free-icons/under-construction"
-            title="under-construction icons"
-          >
-            Under-construction icons created by Freepik - Flaticon
-          </a>
-        </p>
-      </footer>
-    );
+      <p className="mb-0">
+        <a
+          href="https://www.flaticon.com/free-icons/under-construction"
+          title="under-construction icons"
+        >
+          Under-construction icons <br/>created by Freepik - Flaticon
+        </a>
+      </p>
+    </footer>
+  );
+  // return (
+  //     <footer>
+
+  //   </footer>
+  // );
 }
 
 export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 // import './index.css'
 
 createRoot(document.getElementById('root')!).render(
