10fdbfe8ef40961a131d7f130e6e38e625889996
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-04-06 21:09:21 -0300

    Enclosed provisory content of Metallurgy component with a Header, Navbar and a footer

--- src/components/Metallurgy.tsx
+++ src/components/Metallurgy.tsx
@@ -1,19 +1,41 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import NavBar from "./NavBar";
+
 function Metallurgy() {
+  const workInProgressIcon = "./work-in-progress-color.png";
   return (
-    <div className="text-center">
-      <h2 className="display-1 link-body-emphasis mb-1">
-        Scrap Bucket Optimization
-      </h2>
-      <p className="text-center">
-        <a
-          href="https://colab.research.google.com/github/disouzam/ortools-tutorial/blob/colab-changes/eaf-steelmaking-optimization/eaf_optimization_undergrad_exercise.ipynb"
-          target="_blank"
-          rel="noopener"
-        >
-          Optimization of scrap bucket charging in an EAF (undergrad exercise)!
-        </a>
-      </p>
-    </div>
+    <main className="container">
+      <Header />
+      <NavBar />
+      <div className="text-center">
+        <img
+          src={workInProgressIcon}
+          className="rounded"
+          height="100px"
+          alt="Icon of a board saying Work in progress"
+        />
+        <p>
+          <u>Website in construction</u>
+        </p>
+      </div>
+      <div className="text-center">
+        <h2 className="display-1 link-body-emphasis mb-1">
+          Scrap Bucket Optimization
+        </h2>
+        <p className="text-center">
+          <a
+            href="https://colab.research.google.com/github/disouzam/ortools-tutorial/blob/colab-changes/eaf-steelmaking-optimization/eaf_optimization_undergrad_exercise.ipynb"
+            target="_blank"
+            rel="noopener"
+          >
+            Optimization of scrap bucket charging in an EAF (undergrad
+            exercise)!
+          </a>
+        </p>
+      </div>
+      <Footer />
+    </main>
   );
 }
 
