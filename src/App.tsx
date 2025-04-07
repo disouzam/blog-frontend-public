0de70dc0dcd78221fde59875b33d201c770ed969
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-04-06 21:15:46 -0300

    Added three missing routes - Technology, Photography and Cycling with blank content so far

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 
 import Main from "./components/Main";
 import Metallurgy from "./components/Metallurgy";
+import Technology from "./components/Technology";
+import Photography from "./components/Photography";
+import Cycling from "./components/Cycling";
 
 function App() {
   // Make sure to add your actual project id instead of "yourProjectId".
@@ -13,7 +16,10 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/technology" element={<Technology />} />
       <Route path="/metallurgy" element={<Metallurgy />} />
+      <Route path="/photography" element={<Photography />} />
+      <Route path="/cycling" element={<Cycling />} />
     </Routes>
   );
 }
---
+++ src/components/Technology.tsx
@@ -0,0 +1,27 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import NavBar from "./NavBar";
+
+function Technology() {
+  const workInProgressIcon = "./work-in-progress-color.png";
+  return (
+    <main className="container">
+      <Header />
+      <NavBar />
+      <div className="text-center">
+        <img
+          src={workInProgressIcon}
+          className="rounded"
+          height="100px"
+          alt="Icon of a board saying Work in progress"
+        />
+        <p>
+          <u>Website in construction</u>
+        </p>
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+export default Technology;
